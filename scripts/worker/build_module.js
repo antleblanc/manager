4de1587a29f6bdcee5ad039c76905d4e2ab0aaa4
Marie JONES <marie.jones@corp.ovh.com>
2021-02-16 10:33:35 +0100

    test: wip yarn berry
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- scripts/worker/build_module.js
+++ scripts/worker/build_module.js
@@ -2,12 +2,10 @@ const { parentPort, workerData } = require('worker_threads');
 const execa = require('execa');
 
 execa
-  .command(`cd ${workerData.location} && npm run build --if-present`, {
+  .command(`yarn workspaces foreach --include ${workerData.name} run build`, {
     shell: true,
   })
   .then(() => parentPort.postMessage(`done - ${workerData.name}`))
   .catch((err) => {
-    parentPort.postMessage(`error - ${err.message}
-${err.all}`);
     process.exit(err.exitCode);
   });
